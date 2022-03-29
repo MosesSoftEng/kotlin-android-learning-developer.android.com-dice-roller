57520a0ee37c92dbafda58fc8c1f4d0d44c6605d
MosesSoftEng <moses.soft.eng@gmail.com>
2022-03-29 12:00:21 +0300

    Add button listener with toast output

--- app/src/main/java/com/developer/android/dice/roller/MainActivity.kt
+++ app/src/main/java/com/developer/android/dice/roller/MainActivity.kt
@@ -2,10 +2,33 @@ package com.developer.android.dice.roller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*
+         * Button object reference - Create and assign button variable.
+         */
+        val rollButton: Button = findViewById(R.id.button)
+
+        // val button = Button(this);
+
+        /*
+         * Attach click event listener to button.
+         */
+        rollButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+
+        // or
+//        rollButton.setOnClickListener(View.OnClickListener {
+//
+//        })
     }
 }
